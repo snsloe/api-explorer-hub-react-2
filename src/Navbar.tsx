8ad5fd27147d067ba363d1dd67a403e9628576bb
snsloe <ginkosan705@gmail.com>
2025-12-06 01:15:58 +0300

    fix links hash 2.2

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -13,28 +13,26 @@ const Navbar: React.FC = () => {
     }
   };
 
-  const isActive = (path: string) => {
-    return location.pathname === path ? 'active' : '';
-  };
+  const isActive = (path: string) => location.pathname.replace(/^#\/?/, '') === path ? 'active' : '';
 
   return (
     <nav className="navbar">
       <div className="nav-container">
         <h1 className="nav-logo">{getPageTitle()}</h1>
-              <ul className="nav-menu">
-                  <li className="nav-item">
-                      <Link to="" className={`nav-link ${isActive('')}`}>Home</Link>
-                  </li>
-                  <li className="nav-item">
-                      <Link to="github-repos" className={`nav-link ${isActive('github-repos')}`}>GitHub Repos</Link>
-                  </li>
-                  <li className="nav-item">
-                      <Link to="anime-facts" className={`nav-link ${isActive('anime-facts')}`}>Anime Facts</Link>
-                  </li>
-                  <li className="nav-item">
-                      <Link to="dota-rankings" className={`nav-link ${isActive('dota-rankings')}`}>Dota 2 Rankings</Link>
-                  </li>
-              </ul>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className={`nav-link ${isActive('/')}`}>Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/github-repos" className={`nav-link ${isActive('/github-repos')}`}>GitHub Repos</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/anime-facts" className={`nav-link ${isActive('/anime-facts')}`}>Anime Facts</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/dota-rankings" className={`nav-link ${isActive('/dota-rankings')}`}>Dota 2 Rankings</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
