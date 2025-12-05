da82b7d44749d3d919194b5e49d74cd5c5f2c7f8
snsloe <ginkosan705@gmail.com>
2025-12-06 01:52:09 +0300

    fix links hash 2.7

--- src/Home.tsx
+++ src/Home.tsx
@@ -15,21 +15,21 @@ const Home: React.FC = () => {
                         <div className="app-icon"></div>
                         <h3>GitHub Repositories</h3>
                         <p>Откройте для себя самые популярные репозитории GitHub с подробной информацией о звездах, форках и языках программирования.</p>
-                        <Link to="github-repos" className="app-link">Explore Repos</Link>
+                        <Link to="/github-repos" className="app-link">Explore Repos</Link>
                     </div>
 
                     <div className="app-card">
                         <div className="app-icon"></div>
                         <h3>Anime Facts</h3>
                         <p>Получите интересные факты и информацию о популярных аниме, включая рейтинг, жанры, год выхода и описание.</p>
-                        <Link to="anime-facts" className="app-link">Explore Anime</Link>
+                        <Link to="/anime-facts" className="app-link">Explore Anime</Link>
                     </div>
 
                     <div className="app-card">
                         <div className="app-icon"></div>
                         <h3>Dota 2 Rankings</h3>
                         <p>Изучайте рейтинг героев Dota 2. Узнайте лучших героев по популярности и количеству побед. Для загрузки данных необходимо использовать VPN.</p>
-                        <Link to="dota-rankings" className="app-link">View Rankings</Link>
+                        <Link to="/dota-rankings" className="app-link">View Rankings</Link>
                     </div>
                 </div>
             </main>
