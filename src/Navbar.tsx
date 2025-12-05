f20e50ae9f11027b35cc597432ec0788161eb68e
snsloe <ginkosan705@gmail.com>
2025-12-06 01:11:55 +0300

    fix links hash 2

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -21,20 +21,20 @@ const Navbar: React.FC = () => {
     <nav className="navbar">
       <div className="nav-container">
         <h1 className="nav-logo">{getPageTitle()}</h1>
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <Link to="/" className={`nav-link ${isActive('/')}`}>Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/github-repos" className={`nav-link ${isActive('/github-repos')}`}>GitHub Repos</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/anime-facts" className={`nav-link ${isActive('/anime-facts')}`}>Anime Facts</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/dota-rankings" className={`nav-link ${isActive('/dota-rankings')}`}>Dota 2 Rankings</Link>
-          </li>
-        </ul>
+              <ul className="nav-menu">
+                  <li className="nav-item">
+                      <Link to="" className={`nav-link ${isActive('')}`}>Home</Link>
+                  </li>
+                  <li className="nav-item">
+                      <Link to="github-repos" className={`nav-link ${isActive('github-repos')}`}>GitHub Repos</Link>
+                  </li>
+                  <li className="nav-item">
+                      <Link to="anime-facts" className={`nav-link ${isActive('anime-facts')}`}>Anime Facts</Link>
+                  </li>
+                  <li className="nav-item">
+                      <Link to="dota-rankings" className={`nav-link ${isActive('dota-rankings')}`}>Dota 2 Rankings</Link>
+                  </li>
+              </ul>
       </div>
     </nav>
   );
