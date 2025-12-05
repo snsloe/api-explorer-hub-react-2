8fd9f69bd5b521000e939f4368e6f482689cd3b9
snsloe <ginkosan705@gmail.com>
2025-12-06 01:31:50 +0300

    fix links hash 2.4 - close

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,19 @@ import DotaRankings from './DotaRankings';
 import './index.css';
 
 const App: React.FC = () => {
-    return (
-        <Router>
-            <div id="app">
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/github-repos" element={<GitHubRepos />} />
-                    <Route path="/anime-facts" element={<AnimeFacts />} />
-                    <Route path="/dota-rankings" element={<DotaRankings />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div id="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/github-repos" element={<GitHubRepos />} />
+          <Route path="/anime-facts" element={<AnimeFacts />} />
+          <Route path="/dota-rankings" element={<DotaRankings />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
